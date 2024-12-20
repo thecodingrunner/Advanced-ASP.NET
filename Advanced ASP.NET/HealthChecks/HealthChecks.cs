f54c8f1b1935f5a684995fbfe48c24f164617280
thecodingrunner <finn.moffett@gmail.com>
2024-12-20 11:30:58 +0000

    Complete task 2: Add health check for ensuring that teachers exist in Teachers.json file

---
+++ Advanced ASP.NET/HealthChecks/HealthChecks.cs
@@ -0,0 +1,31 @@
+﻿namespace Advanced_ASP.NET.HealthChecks
+{
+    using Microsoft.Extensions.Diagnostics.HealthChecks;
+    using System.Text.Json;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ProductHealthCheck : IHealthCheck
+    {
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var jsonFilePath = "Resources\\Teachers.json";
+
+            var jsonData = await File.ReadAllTextAsync(jsonFilePath);
+
+            var teachersData = JsonSerializer.Deserialize<List<Teacher>>(jsonData);
+
+            int teachers = teachersData.Count();
+
+            if (teachers > 0)
+            {
+                return HealthCheckResult.Healthy($"There are {teachers} teachers available.");
+            }
+            else
+            {
+                return HealthCheckResult.Unhealthy($"There are {teachers} teachers available.");
+            }
+        }
+    }
+}
